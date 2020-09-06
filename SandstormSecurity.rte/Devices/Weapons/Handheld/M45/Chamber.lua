010fe384c78696efbac1da2b443c8bb592736dbe
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2020-09-06 09:42:39 +0200

    M45, remove more unneeded

---
+++ SandstormSecurity.rte/Devices/Weapons/Handheld/M45/Chamber.lua
@@ -0,0 +1,430 @@
+function Create(self)
+
+	self.parentSet = false;
+	
+	-- Sounds --
+	self.addSounds = {["Loop"] = nil};
+	self.addSounds.Loop = {["Variations"] = 4,
+	["Path"] = "SandstormSecurity.rte/Devices/Weapons/Handheld/M45/CompliSoundV2/Add"};
+	
+	self.bassSounds = {["Loop"] = nil};
+	self.bassSounds.Loop = {["Variations"] = 2,
+	["Path"] = "SandstormSecurity.rte/Devices/Weapons/Handheld/M45/CompliSoundV2/Bass"};
+	
+	self.mechSounds = {["Loop"] = nil};
+	self.mechSounds.Loop = {["Variations"] = 4,
+	["Path"] = "SandstormSecurity.rte/Devices/Weapons/Handheld/M45/CompliSoundV2/Mech"};
+	
+	self.noiseSounds = {["Outdoors"] = {["Loop"] = nil, ["End"] = nil},
+	["Indoors"] = {["Loop"] = nil, ["End"] = nil},
+	["bigIndoors"] = {["Loop"] = nil, ["End"] = nil}};
+	self.noiseSounds.Outdoors.End = {["Variations"] = 5,
+	["Path"] = "SandstormSecurity.rte/Devices/Weapons/Handheld/M45/CompliSoundV2/NoiseOutdoorsEnd"};
+	self.noiseSounds.Indoors.End = {["Variations"] = 6,
+	["Path"] = "SandstormSecurity.rte/Devices/Weapons/Handheld/M45/CompliSoundV2/NoiseIndoorsEnd"};
+	self.noiseSounds.bigIndoors.End = {["Variations"] = 6,
+	["Path"] = "SandstormSecurity.rte/Devices/Weapons/Handheld/M45/CompliSoundV2/NoiseBigIndoorsEnd"};
+	
+	self.reflectionSounds = {["Outdoors"] = nil};
+	self.reflectionSounds.Outdoors = {["Variations"] = 3,
+	["Path"] = "SandstormSecurity.rte/Devices/Weapons/Handheld/M45/CompliSoundV2/ReflectionOutdoors"};
+	
+	self.lastAge = self.Age
+	
+	self.Fire = false
+	self.FireTimer = Timer();
+	
+	
+	self.originalSharpLength = self.SharpLength
+	
+	self.originalStanceOffset = Vector(math.abs(self.StanceOffset.X), self.StanceOffset.Y)
+	self.originalSharpStanceOffset = Vector(self.SharpStanceOffset.X, self.SharpStanceOffset.Y)
+	
+	self.rotation = 0
+	self.rotationTarget = 0
+	self.rotationSpeed = 9
+	
+	self.horizontalAnim = 0
+	self.verticalAnim = 0
+	
+	self.angVel = 0
+	self.lastRotAngle = self.RotAngle
+	
+	self.smokeTimer = Timer();
+	self.smokeDelayTimer = Timer();
+	self.canSmoke = false
+	
+	self.reloadTimer = Timer();
+	
+	self.magOutPrepareDelay = 400;
+	self.magOutAfterDelay = 300;
+	self.magInPrepareDelay = 500;
+	self.magInAfterDelay = 200;
+	self.boltForwardPrepareDelay = 350;
+	self.boltForwardAfterDelay = 400;
+	
+	-- phases:
+	-- 0 magout
+	-- 1 magin
+	-- 2 boltforward
+	
+	self.reloadPhase = 0;
+	
+	self.ReloadTime = 9999;
+
+end
+
+function Update(self)
+	self.rotationTarget = 0 -- ZERO IT FIRST AAAA!!!!!
+	
+	if self.ID == self.RootID then
+		self.parent = nil;
+		self.parentSet = false;
+	elseif self.parentSet == false then
+		local actor = MovableMan:GetMOFromID(self.RootID);
+		if actor and IsAHuman(actor) then
+			self.parent = ToAHuman(actor);
+			self.parentSet = true;
+		end
+	end
+	
+	-- Check if switched weapons/hide in the inventory, etc.
+	if self.Age > (self.lastAge + TimerMan.DeltaTimeSecs * 2000) then
+		if self.Fire then
+			self.Fire = false
+			if self.Magazine then
+				self.Magazine.RoundCount = self.Magazine.RoundCount + 1
+			end
+		end
+	end
+	self.lastAge = self.Age + 0
+	
+	-- Smoothing
+	local min_value = -math.pi;
+	local max_value = math.pi;
+	local value = self.RotAngle - self.lastRotAngle
+	local result;
+	local ret = 0
+	
+	local range = max_value - min_value;
+	if range <= 0 then
+		result = min_value;
+	else
+		ret = (value - min_value) % range;
+		if ret < 0 then ret = ret + range end
+		result = ret + min_value;
+	end
+	
+	self.lastRotAngle = self.RotAngle
+	self.angVel = (result / TimerMan.DeltaTimeSecs) * self.FlipFactor
+	
+	if self.FiredFrame then
+	
+		self.Fire = true;
+		self.FireTimer:Reset();
+	
+		self.angVel = self.angVel + RangeRand(0.7,1.1) * 30
+		
+		self.canSmoke = true
+		self.smokeTimer:Reset()
+		
+		local muzzleFlash = CreateAttachable("Muzzle Flash Pistol", "Base.rte");
+		muzzleFlash.ParentOffset = self.MuzzleOffset
+		muzzleFlash.Lifetime = TimerMan.DeltaTimeSecs * 1300
+		muzzleFlash.Frame = math.random(0, muzzleFlash.FrameCount - 1);
+		self:AddAttachable(muzzleFlash);
+		
+		if self.noiseEndSound then
+			if self.noiseEndSound:IsBeingPlayed() then
+				self.noiseEndSound:Stop(-1)
+			end
+		end
+		
+		if self.reflectionSound then
+			if self.reflectionSound:IsBeingPlayed() then
+				self.reflectionSound:Stop(-1)
+			end
+		end
+
+		local outdoorRays = 0;
+		
+		local indoorRays = 0;
+		
+		local bigIndoorRays = 0;
+
+		if self.parent:IsPlayerControlled() then
+			local Vector2 = Vector(0,-700); -- straight up
+			local Vector2Left = Vector(0,-700):RadRotate(45*(math.pi/180));
+			local Vector2Right = Vector(0,-700):RadRotate(-45*(math.pi/180));			
+			local Vector2SlightLeft = Vector(0,-700):RadRotate(22.5*(math.pi/180));
+			local Vector2SlightRight = Vector(0,-700):RadRotate(-22.5*(math.pi/180));		
+			local Vector3 = Vector(0,0); -- dont need this but is needed as an arg
+			local Vector4 = Vector(0,0); -- dont need this but is needed as an arg
+
+			self.ray = SceneMan:CastObstacleRay(self.Pos, Vector2, Vector3, Vector4, self.RootID, self.Team, 128, 7);
+			self.rayRight = SceneMan:CastObstacleRay(self.Pos, Vector2Right, Vector3, Vector4, self.RootID, self.Team, 128, 7);
+			self.rayLeft = SceneMan:CastObstacleRay(self.Pos, Vector2Left, Vector3, Vector4, self.RootID, self.Team, 128, 7);			
+			self.raySlightRight = SceneMan:CastObstacleRay(self.Pos, Vector2SlightRight, Vector3, Vector4, self.RootID, self.Team, 128, 7);
+			self.raySlightLeft = SceneMan:CastObstacleRay(self.Pos, Vector2SlightLeft, Vector3, Vector4, self.RootID, self.Team, 128, 7);
+			
+			self.rayTable = {self.ray, self.rayRight, self.rayLeft, self.raySlightRight, self.raySlightLeft};
+		else
+			local Vector2 = Vector(0,-700); -- straight up
+			local Vector3 = Vector(0,0); -- dont need this but is needed as an arg
+			local Vector4 = Vector(0,0); -- dont need this but is needed as an arg		
+			self.ray = SceneMan:CastObstacleRay(self.Pos, Vector2, Vector3, Vector4, self.RootID, self.Team, 128, 7);
+			
+			self.rayTable = {self.ray};
+		end
+		
+		for _, rayLength in ipairs(self.rayTable) do
+			if rayLength < 0 then
+				outdoorRays = outdoorRays + 1;
+			elseif rayLength > 170 then
+				bigIndoorRays = bigIndoorRays + 1;
+			else
+				indoorRays = indoorRays + 1;
+			end
+		end
+		
+		self.bassSound = AudioMan:PlaySound(self.bassSounds.Loop.Path .. math.random(1, self.bassSounds.Loop.Variations) .. ".wav", self.Pos, -1, 0, 130, 1, 450, false);			
+		self.mechSound = AudioMan:PlaySound(self.mechSounds.Loop.Path .. math.random(1, self.mechSounds.Loop.Variations) .. ".wav", self.Pos, -1, 0, 130, 1, 450, false);
+		
+		if outdoorRays >= 2 then
+			self.noiseEndSound = AudioMan:PlaySound(self.noiseSounds.Outdoors.End.Path .. math.random(1, self.noiseSounds.Outdoors.End.Variations) .. ".wav", self.Pos, -1, 0, 130, 1, 450, false);
+			self.reflectionSound = AudioMan:PlaySound(self.reflectionSounds.Outdoors.Path .. math.random(1, self.reflectionSounds.Outdoors.Variations) .. ".wav", self.Pos, -1, 0, 130, 1, 450, false);
+		elseif math.max(outdoorRays, bigIndoorRays, indoorRays) == indoorRays then
+			self.noiseEndSound = AudioMan:PlaySound(self.noiseSounds.Indoors.End.Path .. math.random(1, self.noiseSounds.Indoors.End.Variations) .. ".wav", self.Pos, -1, 0, 130, 1, 450, false);
+		else -- bigIndoor
+			self.noiseEndSound = AudioMan:PlaySound(self.noiseSounds.bigIndoors.End.Path .. math.random(1, self.noiseSounds.bigIndoors.End.Variations) .. ".wav", self.Pos, -1, 0, 130, 1, 450, false);
+		end
+
+	
+		self.addSound = AudioMan:PlaySound(self.addSounds.Loop.Path .. math.random(1, self.addSounds.Loop.Variations) .. ".wav", self.Pos, -1, 0, 130, 1, 450, false);
+		
+		local bullet = CreateMOSRotating("Bullet M45");
+		bullet.Pos = self.Pos + Vector(self.MuzzleOffset.X * self.FlipFactor, self.MuzzleOffset.Y):RadRotate(self.RotAngle + RangeRand(-0.05,0.05));
+		bullet.Vel = self.Vel + Vector(1 * self.FlipFactor,0):RadRotate(self.RotAngle) * 180; -- BULLET SPEED
+		bullet.RotAngle = self.RotAngle + (math.pi * (-self.FlipFactor + 1) / 2)
+		bullet:SetNumberValue("WoundDamageMultiplier", 2.0)
+		bullet:SetNumberValue("AlwaysTracer", math.random(0,1))
+		bullet:SetNumberValue("NoSmoke", 1)
+		if self.parent then
+			bullet.Team = self.parent.Team;
+			bullet.IgnoresTeamHits = true;
+		end
+		
+		local casing
+		casing = CreateMOSParticle("Casing");
+		casing.Pos = self.Pos+Vector(0,-3):RadRotate(self.RotAngle);
+		casing.Vel = self.Vel+Vector(-math.random(2,4)*self.FlipFactor,-math.random(3,4)):RadRotate(self.RotAngle);
+		MovableMan:AddParticle(casing);
+		MovableMan:AddParticle(bullet);
+		
+		self.Fire = false
+		
+		self.canShoot = false
+	else
+		self.Fire = false;
+	end
+	
+	-- PAWNIS RELOAD ANIMATION HERE
+	if self:IsReloading() then
+	
+		if self.parent then
+			self.parent:GetController():SetState(Controller.AIM_SHARP,false);
+		end
+
+		if self.reloadPhase == 0 then
+			self.reloadDelay = self.magOutPrepareDelay;
+			self.afterDelay = self.magOutAfterDelay;			
+			self.prepareSoundPath = 
+			"SandstormSecurity.rte/Devices/Weapons/Handheld/M45/Sounds/MagOutPrepare1";
+			self.afterSoundPath = 
+			"SandstormSecurity.rte/Devices/Weapons/Handheld/M45/Sounds/MagOut1";
+			
+		elseif self.reloadPhase == 1 then
+			self.reloadDelay = self.magInPrepareDelay;
+			self.afterDelay = self.magInAfterDelay;
+			self.prepareSoundPath = 
+			"SandstormSecurity.rte/Devices/Weapons/Handheld/M45/Sounds/MagInPrepare1";
+			self.afterSoundPath = 
+			"SandstormSecurity.rte/Devices/Weapons/Handheld/M45/Sounds/MagIn1";
+			
+		elseif self.reloadPhase == 2 then
+			self.Frame = 2;
+			self.reloadDelay = self.boltForwardPrepareDelay;
+			self.afterDelay = self.boltForwardAfterDelay;
+			self.prepareSoundPath = nil;
+			self.afterSoundPath = 
+			"SandstormSecurity.rte/Devices/Weapons/Handheld/M45/Sounds/BoltForward1";
+			
+		end
+		
+		if self.prepareSoundPlayed ~= true then
+			self.prepareSoundPlayed = true;
+			if self.prepareSoundPath then
+				self.prepareSound = AudioMan:PlaySound(self.prepareSoundPath .. ".wav", self.Pos, -1, 0, 130, 1, 250, false);
+			end
+		end
+	
+		if self.reloadTimer:IsPastSimMS(self.reloadDelay) then
+		
+			if self.reloadPhase == 0 then
+				self:SetNumberValue("MagRemoved", 1);
+			elseif self.reloadPhase == 1 then
+				self:RemoveNumberValue("MagRemoved");
+			elseif self.reloadPhase == 2 then
+				if self.reloadTimer:IsPastSimMS(self.reloadDelay + ((self.afterDelay/5)*1.5)) then
+					self.Frame = 0;
+				elseif self.reloadTimer:IsPastSimMS(self.reloadDelay + ((self.afterDelay/5)*1)) then
+					self.Frame = 1;
+				end
+			end
+			
+			if self.afterSoundPlayed ~= true then
+				
+				if self.reloadPhase == 2 then
+					self.horizontalAnim = self.horizontalAnim - 3
+					self.angVel = self.angVel - 37
+				end
+			
+				if self.reloadPhase == 0 then
+					self.phaseOnStop = 1;
+					local fake
+					fake = CreateMOSRotating("Fake Magazine MOSRotating M45");
+					fake.Pos = self.Pos + Vector(-3 * self.FlipFactor, 3):RadRotate(self.RotAngle);
+					fake.Vel = self.Vel + Vector(0.5*self.FlipFactor, 3):RadRotate(self.RotAngle);
+					fake.RotAngle = self.RotAngle;
+					fake.AngularVel = self.AngularVel + (-1*self.FlipFactor);
+					fake.HFlipped = self.HFlipped;
+					MovableMan:AddParticle(fake);
+					
+					self.verticalAnim = self.verticalAnim + 1
+				elseif self.reloadPhase == 1 then
+					if self.chamberOnReload then
+						self.phaseOnStop = 2;
+					else
+						self.ReloadTime = 0; -- done! no after delay if non-chambering reload.
+						self.reloadPhase = 0;
+						self.phaseOnStop = nil;
+					end
+					self:RemoveNumberValue("MagRemoved");
+					
+					self.horizontalAnim = self.horizontalAnim + 3
+					self.verticalAnim = self.verticalAnim - 1
+					self.angVel = self.angVel + 15
+				else
+					self.phaseOnStop = nil;
+				end
+			
+				self.afterSoundPlayed = true;
+				if self.afterSoundPath then
+					self.afterSound = AudioMan:PlaySound(self.afterSoundPath .. ".wav", self.Pos, -1, 0, 130, 1, 250, false);
+				end
+			end
+			if self.reloadTimer:IsPastSimMS(self.reloadDelay + self.afterDelay) then
+				self.reloadTimer:Reset();
+				self.prepareSoundPlayed = false;
+				self.afterSoundPlayed = false;
+				if self.chamberOnReload and self.reloadPhase == 1 then
+					self.reloadPhase = self.reloadPhase + 1;
+				elseif self.reloadPhase == 1 or self.reloadPhase == 2 then
+					self.ReloadTime = 0;
+					self.reloadPhase = 0;
+				else
+					self.reloadPhase = self.reloadPhase + 1;
+				end
+			end
+		end		
+	else
+		
+		self.reloadTimer:Reset();
+		self.prepareSoundPlayed = false;
+		self.afterSoundPlayed = false;
+		if self.reloadPhase == 3 then
+			self.reloadPhase = 2;
+		end
+		if self.phaseOnStop then
+			self.reloadPhase = self.phaseOnStop;
+			self.phaseOnStop = nil;
+		end
+		self.ReloadTime = 9999;
+		-- SLIDE animation when firing
+		-- don't ask, math magic
+		if self.Magazine and self.Magazine.RoundCount < 1 or not self.Magazine then
+			self.chamberOnReload = true;
+			self.Frame = 2;
+		else
+			local f = math.max(1 - math.min((self.FireTimer.ElapsedSimTimeMS) / 100, 1), 0)
+			self.Frame = math.floor(f * 2 + 0.55);
+		end
+	end
+	
+	if self:DoneReloading() == true and self.chamberOnReload then
+		self.Magazine.RoundCount = 7
+		self.chamberOnReload = false;
+	elseif self:DoneReloading() then
+		self.Magazine.RoundCount = 8
+		self.chamberOnReload = false;
+	end	
+	-- PAWNIS RELOAD ANIMATION HERE
+	
+	-- Animation
+	if self.parent then
+		self.horizontalAnim = math.floor(self.horizontalAnim / (1 + TimerMan.DeltaTimeSecs * 24.0) * 1000) / 1000
+		self.verticalAnim = math.floor(self.verticalAnim / (1 + TimerMan.DeltaTimeSecs * 15.0) * 1000) / 1000
+		
+		local stance = Vector()
+		stance = stance + Vector(-1,0) * self.horizontalAnim -- Horizontal animation
+		stance = stance + Vector(0,5) * self.verticalAnim -- Vertical animation
+		
+		self.rotationTarget = self.rotationTarget + (self.angVel * 3)
+		self.rotation = (self.rotation + self.rotationTarget * TimerMan.DeltaTimeSecs * self.rotationSpeed) / (1 + TimerMan.DeltaTimeSecs * self.rotationSpeed)
+		local total = math.rad(self.rotation) * self.FlipFactor
+		
+		self.RotAngle = self.RotAngle + total;
+		self:SetNumberValue("MagRotation", total);
+		
+		local jointOffset = Vector(self.JointOffset.X * self.FlipFactor, self.JointOffset.Y):RadRotate(self.RotAngle);
+		local offsetTotal = Vector(jointOffset.X, jointOffset.Y):RadRotate(-total) - jointOffset
+		self.Pos = self.Pos + offsetTotal;
+		self:SetNumberValue("MagOffsetX", offsetTotal.X);
+		self:SetNumberValue("MagOffsetY", offsetTotal.Y);
+		
+		self.StanceOffset = Vector(self.originalStanceOffset.X, self.originalStanceOffset.Y) + stance
+		self.SharpStanceOffset = Vector(self.originalSharpStanceOffset.X, self.originalSharpStanceOffset.Y) + stance
+	end
+	
+	if self.canSmoke and not self.smokeTimer:IsPastSimMS(1500) then
+		--[[
+		local poof = CreateMOPixel("Real Bullet Micro Smoke Ball "..math.random(1,4), "Sandstorm.rte");
+		poof.Pos = self.Pos + Vector(self.MuzzleOffset.X * self.FlipFactor, self.MuzzleOffset.Y):RadRotate(self.RotAngle) + Vector(RangeRand(-1,1), RangeRand(-1,1));
+		poof.Lifetime = poof.Lifetime * RangeRand(0.2, 1.3) * 0.9;
+		poof.Vel = self.Vel * 0.1
+		poof.GlobalAccScalar = RangeRand(0.9, 1.0) * -0.4; -- Go up and down
+		MovableMan:AddParticle(poof);
+		]]
+		if self.smokeDelayTimer:IsPastSimMS(120) then
+			
+			local poof = math.random(1,2) < 2 and CreateMOSParticle("Tiny Smoke Ball 1") or CreateMOPixel("Real Bullet Micro Smoke Ball "..math.random(1,4), "Sandstorm.rte");
+			poof.Pos = self.Pos + Vector(self.MuzzleOffset.X * self.FlipFactor, self.MuzzleOffset.Y):RadRotate(self.RotAngle);
+			poof.Lifetime = poof.Lifetime * RangeRand(0.3, 1.3) * 0.9;
+			poof.Vel = self.Vel * 0.1
+			poof.GlobalAccScalar = RangeRand(0.9, 1.0) * -0.4; -- Go up and down
+			MovableMan:AddParticle(poof);
+			self.smokeDelayTimer:Reset()
+			
+			--[[
+			for i = 0, 2 do
+				local poof = i == 0 and CreateMOSParticle("Tiny Smoke Ball 1") or CreateMOPixel("Real Bullet Micro Smoke Ball "..math.random(1,4), "Sandstorm.rte");
+				poof.Pos = self.Pos + Vector(self.MuzzleOffset.X * self.FlipFactor, self.MuzzleOffset.Y):RadRotate(self.RotAngle) + Vector(RangeRand(-1,1), RangeRand(-1,1));
+				poof.Lifetime = poof.Lifetime * RangeRand(0.7, 1.7) * 0.9;
+				--poof.Vel = Vector(RangeRand(-1,1), RangeRand(-1,1)) * 0.5
+				poof.GlobalAccScalar = RangeRand(0.9, 1.0) * -0.4; -- Go up and down
+				MovableMan:AddParticle(poof);
+				self.smokeDelayTimer:Reset()
+			end]]
+		end
+	end
+end
