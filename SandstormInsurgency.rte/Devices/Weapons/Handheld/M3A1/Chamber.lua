a1063122b29582013d97b3905324c89e3d2c1876
filipex2000 <filipex570@gmail.com>
2020-10-27 12:37:51 +0100

    saiga and grease gun sprites

--- SandstormInsurgency.rte/Devices/Weapons/Handheld/M3A1/Chamber.lua
+++ SandstormInsurgency.rte/Devices/Weapons/Handheld/M3A1/Chamber.lua
@@ -275,7 +275,7 @@ function Update(self)
 					self.phaseOnStop = 1;
 					local fake
 					fake = CreateMOSRotating("Fake Magazine MOSRotating M3A1");
-					fake.Pos = self.Pos + Vector(0, 2):RadRotate(self.RotAngle);
+					fake.Pos = self.Pos + Vector(1*self.FlipFactor, 5):RadRotate(self.RotAngle);
 					fake.Vel = self.Vel + Vector(0.5*self.FlipFactor, 3):RadRotate(self.RotAngle);
 					fake.RotAngle = self.RotAngle;
 					fake.AngularVel = self.AngularVel + (-1*self.FlipFactor);
